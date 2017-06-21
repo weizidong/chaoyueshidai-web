6a03a551f143586d941a52bfe5c16d6948e1959b
weizidong <123456>
2017-06-21 17:25:38 +0800

    初始化

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,5 +1,4 @@
 import ajax from './ajax'
 const ROOT = '/rest/user/'
-export const login = ({username = '', pwd = ''} = {}) => {
-  ajax.post(`${ROOT}login`, {username, pwd})
-}
+// 登录
+export const login = ({username = '', pwd = ''} = {}) => ajax.post(`${ROOT}login`, {username, pwd})
