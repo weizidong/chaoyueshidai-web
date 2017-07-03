605bddec7cb25a68c9e9109079a0a624d8bc7b0c
weizidong <123456>
2017-07-03 11:45:38 +0800

    搭建

--- src/view/manage/config/ConfigEdit.vue
+++ src/view/manage/config/ConfigEdit.vue
@@ -5,18 +5,14 @@
 <template>
   <el-tabs v-model="activeName" class="config_edit" type="card" @tab-click="tabClick">
     <el-tab-pane label="项目案例类型" name="example">
-      <el-form ref="exampleForm" class="form" :inline="true" :model="data" label-width="120">
+      <el-form ref="exampleForm" class="form" inline :model="data">
         <el-row>
-          <el-form-item>
-            <i class="el-icon-plus add" @click="$set(data.example,data.example.length,{name:'',show:false,icon:''})"></i>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          </el-form-item>
+          <i class="el-icon-plus add" @click="$set(data.example,data.example.length,{name:'',show:false,icon:''})"></i>
+          <el-button type="primary" @click="onSubmit">保存</el-button>
         </el-row>
         <transition-group name="list-complete" tag="p" class="list">
           <div v-for="(example,i) in data.example" :key="example" class="list-complete-item">
-            <el-form-item :label="(i+1)+'、类型名称：'" style="width: 30%;text-align: center">
+            <el-form-item :label="(i+1)+'、类型名称：'" style="width: 35%;text-align: center">
               <el-input v-model="example.name"></el-input>
             </el-form-item>
             <el-form-item label="是否显示：" style="width: 15%;text-align: center">
@@ -25,9 +21,9 @@
             <el-form-item label="显示图标：" style="width: 15%;text-align: center">
               <Avatar width="32" height="32"/>
             </el-form-item>
-            <el-form-item label="路径设置：" style="width: 30%;text-align: center">
-              <el-input v-model="example.key" style="width: 230px">
-                <template slot="prepend">/view/manage/example/</template>
+            <el-form-item label="路径设置：" style="width: 25%;text-align: center">
+              <el-input v-model="example.key" style="width: 130px">
+                <template slot="prepend">./example/</template>
               </el-input>
             </el-form-item>
             <el-form-item style="width: 5%;text-align: center">
@@ -38,18 +34,14 @@
       </el-form>
     </el-tab-pane>
     <el-tab-pane label="我的笔记类型" name="note">
-      <el-form ref="noteForm" class="form" :inline="true" :model="data" label-width="120">
+      <el-form ref="noteForm" class="form" inline :model="data">
         <el-row>
-          <el-form-item>
-            <i class="el-icon-plus add" @click="$set(data.note,data.note.length,{name:'',show:false,icon:''})"></i>
-          </el-form-item>
-          <el-form-item>
-            <el-button type="primary" @click="onSubmit">立即创建</el-button>
-          </el-form-item>
+          <i class="el-icon-plus add" @click="$set(data.note,data.note.length,{name:'',show:false,icon:''})"></i>
+          <el-button type="primary" @click="onSubmit">保存</el-button>
         </el-row>
         <transition-group name="list-complete" tag="p" class="list">
           <div v-for="(note,i) in data.note" :key="note" class="list-complete-item">
-            <el-form-item :label="(i+1)+'、类型名称：'" style="width: 30%;text-align: center">
+            <el-form-item :label="(i+1)+'、类型名称：'" style="width: 35%;text-align: center">
               <el-input v-model="note.name"></el-input>
             </el-form-item>
             <el-form-item label="是否显示：" style="width: 15%;text-align: center">
@@ -58,12 +50,12 @@
             <el-form-item label="显示图标：" style="width: 15%;text-align: center">
               <Avatar width="32" height="32"/>
             </el-form-item>
-            <el-form-item label="路径匹配：" style="width: 30%;text-align: center">
-              <el-input v-model="note.key" style="width: 230px">
-                <template slot="prepend">/view/manage/note/</template>
+            <el-form-item label="路径匹配：" style="width: 25%;text-align: center">
+              <el-input v-model="note.key" style="width: 130px">
+                <template slot="prepend">./note/</template>
               </el-input>
             </el-form-item>
-            <el-form-item style="width: 5%;text-align: center">
+            <el-form-item style="width:5%;text-align: center">
               <i class="el-icon-delete2 delete" @click="$delete(data.note,i)"></i>
             </el-form-item>
           </div>
@@ -71,7 +63,41 @@
       </el-form>
     </el-tab-pane>
     <el-tab-pane label="关于我的介绍" name="3">
-      关于我的介绍开发中。。。
+      <el-form ref="mineForm" class="form" :model="data">
+        <el-row>
+          <i class="el-icon-plus add" @click="$set(data.mine,data.mine.length,{type:'',list:[]})"></i>
+          <el-button type="primary" @click="onSubmit">保存</el-button>
+        </el-row>
+        <transition-group name="list-complete" tag="p" class="list">
+          <el-form-item v-for="(mine,i) in data.mine" :key="mine" class="list-complete-item">
+            <el-row>
+              <el-col :span="3" style="width: 10%">
+                <el-input v-model="mine.type" placeholder="类别" style="width: 100px">
+                  <template slot="append">：</template>
+                </el-input>
+              </el-col>
+              <el-col :span="18" style="width: 80%">
+                <div v-if="!mine.list.length" style="border: 1px solid #bfcbd9;padding: 5px;border-radius: 5px;">请添加内容！</div>
+                <transition-group name="list-complete" tag="p" class="list" style="margin: 0">
+                  <el-row v-for="(list,i) in mine.list" :key="list" class="list-complete-item">
+                    <el-form-item label="标题：" style="">
+                      <el-input v-model="list.title"></el-input>
+                    </el-form-item>
+                    <el-form-item label="内容：" style="">
+                      <el-input v-model="list.content"></el-input>
+                    </el-form-item>
+                    <i class="el-icon-delete2 delete" style="position: absolute;top: 15px;left: 50px" @click="$delete(mine.list,i)"></i>
+                  </el-row>
+                </transition-group>
+              </el-col>
+              <el-col :span="3" style="width: 10%">
+                <i class="el-icon-plus add" @click="$set(mine.list,mine.list.length,{title:'',content:''})"></i>
+                <i class="el-icon-delete2 delete" @click="$delete(data.mine,i)"></i>
+              </el-col>
+            </el-row>
+          </el-form-item>
+        </transition-group>
+      </el-form>
     </el-tab-pane>
     <el-tab-pane label="微信关注设置" name="4">
       微信关注设置开发中。。。
@@ -87,7 +113,7 @@
     data () {
       return {
         activeName: '',
-        data: {example: [], note: [], wechat: {}},
+        data: {example: [], note: [], wechat: {}, mine: []},
       }
     },
     components: {Avatar},
