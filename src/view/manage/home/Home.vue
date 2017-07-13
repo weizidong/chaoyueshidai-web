3146dfd1cae9564bd81ec61547327d2078952968
weizidong <123456>
2017-07-13 11:28:24 +0800

    搭建

--- src/view/manage/home/Home.vue
+++ src/view/manage/home/Home.vue
@@ -3,7 +3,7 @@
 </style>
 
 <template>
-  <div class="home">
+  <div class="home" style="margin: 50% 0 0 50%;transform: translate(-50%,-50%);width: 400px;">
     <img src="/static/img/logo01.png">
     <div>欢迎使用本系统！</div>
     <div>其他功能还在陆续开发中，敬请期待。。。</div>
