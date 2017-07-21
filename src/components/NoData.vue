48d8cf64b5fe8b1a06f551d29f07275fe1a644b9
weizidong <123456>
2017-07-21 18:45:16 +0800

    搭建

---
+++ src/components/NoData.vue
@@ -0,0 +1,23 @@
+<style lang="less" scoped>
+  .noData {
+    div{font-size:24px;}
+  }
+</style>
+<template>
+  <div class="noData" v-if="show">
+    <img src="/static/img/nodata.png">
+    <div>暂无数据！</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NoData',
+    props: {
+      show: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  }
+</script>
--- src/view/web/note/Note.vue
+++ src/view/web/note/Note.vue
@@ -7,22 +7,19 @@
     <transition name="title" mode="out-in">
       <div class="title" v-for="(v,i) in noteType" :key="i" v-if="$route.params.type == i">{{v}}</div>
     </transition>
-    <el-row>
-      <el-col :span="6" v-for="i in 4" :key="i">
-        <transition-group name="noe-complete" tag="div">
-          <el-card v-if="k%4 == i-1" :body-style="{ padding: '0' }" v-for="(e,k) in data.list" :key="e" class="noe-complete-item">
-            <div @click="$router.push({name: 'noteInfo', params: {id:e.id}})" class="title">
-              <img :src="e.pic">
-              <div>{{e.title}}</div>
-            </div>
-            <div class="bottom">
-              <time class="time">{{ dateFilter(e.created) }}</time>
-              <div>作者：<span @click="$router.push({name: 'noteUser', params: {id:e.userid}})"><img :src="e.avatarUrl">{{e.userName}}</span></div>
-            </div>
-          </el-card>
-        </transition-group>
-      </el-col>
-    </el-row>
+    <transition-group name="noe-complete" tag="div" mode="out-in" style="text-align: left">
+      <el-card :body-style="{ padding: '0' }" v-for="(e,k) in data.list" :key="e" class="noe-complete-item">
+        <div @click="$router.push({name: 'noteInfo', params: {id:e.id}})" class="title">
+          <img :src="e.pic.indexOf('/userfiles/')>0?e.pic: '/static/img/none.png'">
+          <div>{{e.title}}</div>
+        </div>
+        <div class="bottom">
+          <time class="time">{{ dateFilter(e.created) }}</time>
+          <div>作者：<span @click="$router.push({name: 'noteUser', params: {id:e.userid}})"><img :src="e.avatarUrl">{{e.userName}}</span></div>
+        </div>
+      </el-card>
+    </transition-group>
+    <NoData :show="data.list.length==0"></NoData>
   </div>
 </template>
 
@@ -30,8 +27,10 @@
   import {findNoteApi} from '../../../api/noteApi'
   import {dateFilter} from '../../../constant/filter'
   import {noteType} from '../../../constant'
+  import NoData from '../../../components/NoData.vue'
   export default {
     name: 'note',
+    components: {NoData},
     data () {
       return {
         noteType,
@@ -44,16 +43,13 @@
       findList () {
         const {type} = this.$route.params
         findNoteApi(type, this.data, 0).then((data) => {
-          [0, 0, 0, 0].forEach(() => {
-            data.list = [...data.list, ...data.list]
-          })
           this.data = data
         })
       },
     },
     beforeRouteUpdate (to, from, next) {
       next()
-      this.data = {page: 1, pageSize: 20, list: [], all: 0}
+      this.data = {page: 1, pageSize: 20, all: 0}
       if (!to.query.page) {
         this.findList()
       }
